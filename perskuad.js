fae33e844b793c919cb515f42bb0d22777d75297
BimbelTuntas <77866408+irzalgit@users.noreply.github.com>
2023-02-13 11:00:42 +0700

    Add files via upload

---
+++ perskuad.js
@@ -0,0 +1,341 @@
+function kirimperskuad() {
+  let poin = 0;
+  const jno71 = document.querySelector("#jno71");
+  const jno72 = document.querySelector("#jno72");
+  const jno73 = document.querySelector("#jno73");
+  const jno74 = document.querySelector("#jno74");
+  const jno75 = document.querySelector("#jno75");
+  const jno76 = document.querySelector("#jno76");
+  const jno77 = document.querySelector("#jno77");
+  const jno78 = document.querySelector("#jno78");
+  const jno79 = document.querySelector("#jno79");
+  const jno80 = document.querySelector("#jno80");
+  const jno81 = document.querySelector("#jno81");
+  const jno82 = document.querySelector("#jno82");
+  const jno83 = document.querySelector("#jno83");
+  const jno84 = document.querySelector("#jno84");
+  const jno85 = document.querySelector("#jno85");
+  const jno86 = document.querySelector("#jno86");
+  const jno87 = document.querySelector("#jno87");
+  const jno88 = document.querySelector("#jno88");
+  const jno89 = document.querySelector("#jno89");
+  const jno90 = document.querySelector("#jno90");
+  const jno91 = document.querySelector("#jno91");
+  const jno92 = document.querySelector("#jno92");
+  const jno93 = document.querySelector("#jno93");
+  const jno94 = document.querySelector("#jno94");
+  const jno95 = document.querySelector("#jno95");
+  const jno121 = document.querySelector("#jno121");
+  const jno122 = document.querySelector("#jno122");
+  const jno123 = document.querySelector("#jno123");
+  const jno124 = document.querySelector("#jno124");
+  const jno125 = document.querySelector("#jno125");
+  const jno126 = document.querySelector("#jno126");
+  const jno127 = document.querySelector("#jno127");
+  const jno128 = document.querySelector("#jno128");
+  const jno129 = document.querySelector("#jno129");
+  const jno130 = document.querySelector("#jno130");
+
+  const sno121 = document.querySelector("#sno121");
+  const sno122 = document.querySelector("#sno122");
+
+  const nilaip2 = document.querySelector("#nilaipers");
+
+  if (jno71.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno71.innerHTML = response;
+
+  if (jno72.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno72.innerHTML = response;
+
+  if (jno73.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno73.innerHTML = response;
+
+  if (jno74.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno74.innerHTML = response;
+
+  if (jno75.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno75.innerHTML = response;
+
+  if (jno76.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno76.innerHTML = response;
+
+  if (jno77.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno77.innerHTML = response;
+
+  if (jno78.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno78.innerHTML = response;
+
+  if (jno79.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno79.innerHTML = response;
+
+  if (jno80.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno80.innerHTML = response;
+
+  if (jno81.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno81.innerHTML = response;
+
+  if (jno82.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno82.innerHTML = response;
+
+  if (jno83.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno83.innerHTML = response;
+
+  if (jno84.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno84.innerHTML = response;
+
+  if (jno85.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno85.innerHTML = response;
+
+  if (jno86.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno86.innerHTML = response;
+
+  if (jno87.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno87.innerHTML = response;
+
+  if (jno88.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno88.innerHTML = response;
+
+  if (jno89.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno89.innerHTML = response;
+
+  if (jno90.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno90.innerHTML = response;
+
+  if (jno91.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno91.innerHTML = response;
+
+  if (jno92.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno92.innerHTML = response;
+
+  if (jno93.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno93.innerHTML = response;
+
+  if (jno94.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno94.innerHTML = response;
+
+  if (jno95.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno95.innerHTML = response;
+
+  if (jno121.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno121.innerHTML = response;
+
+  if (jno122.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno122.innerHTML = response;
+
+  if (jno123.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno123.innerHTML = response;
+
+  if (jno124.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno124.innerHTML = response;
+
+  if (jno125.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno125.innerHTML = response;
+
+  if (jno126.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno126.innerHTML = response;
+
+  if (jno127.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno127.innerHTML = response;
+
+  if (jno128.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno128.innerHTML = response;
+
+  if (jno129.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno129.innerHTML = response;
+
+  if (jno130.checked) {
+    response = "ok";
+    poin = poin + 10;
+  } else {
+    response = "x";
+  }
+  rno130.innerHTML = response;
+
+  if (sno121.value == "ponens") {
+    response = "ok";
+    poin = poin + 25;
+  } else {
+    response = "x";
+  }
+  rsno121.innerHTML = response;
+
+  if (sno122.value == "kontraposisi") {
+    response = "ok";
+    poin = poin + 25;
+  } else {
+    response = "x";
+  }
+  rsno122.innerHTML = response;
+
+  nilaip2.innerHTML = poin;
+}
