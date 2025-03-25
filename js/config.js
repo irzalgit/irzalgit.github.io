04bfc60eff2a0968b979fdb89f2809f2edbee7f0
BimbelTuntas <77866408+irzalgit@users.noreply.github.com>
2025-03-25 21:38:15 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// config.js (JANGAN diunggah ke GitHub)
+const firebaseConfig = {
+    apiKey: "AIzaSyCu4GQa-EOaPhpQP_y_QcsJxs05adQU2-M",
+    authDomain: "math314-app.firebaseapp.com",
+    databaseURL: "https://math314-app-default-rtdb.firebaseio.com/",
+    projectId: "math314-app",
+    storageBucket: "math314-app.appspot.com",
+    messagingSenderId: "9019551034",
+    appId: "1:9019551034:web:de0c5e2b8b20c18ca0b403"
+};
